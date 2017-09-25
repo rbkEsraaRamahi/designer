7f5485b80cf64ab949c55389c56a1c422b5d8234
rbkEsraaRamahi <israma7i@yahoo.com>
2017-09-25 18:34:50 +0300

    2nd try

--- index.jsx
+++ index.jsx
@@ -5,19 +5,26 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			yourStatment: ''
+			yourStatment: '',
+			repeats: ''
 		}
 	}
 
 	repeat(){
-	this.props.
+	(this.props.yourStatment).repeat(this.props.repeats)
 }
 
 render(){
 	return (<div>
 		<h3> Write your repeated statment </h3>
-		<input value = {this.state.yourStatment} />
+		<input value = {this.state.yourStatment}, />
+		<input value = {this.state.repeats} />
 		<button onClick = {this.repeat}> do it </button>
+	</div>
+	<div>
+		<lu>
+			repeat()
+		</lu>
 	</div>)
 }
 
@@ -25,7 +32,6 @@ render(){
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
 
 
 
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require ('express')
 var app = express()
 
 app.get('/', function (req, res){
-	res.send('insert your repeated sentence :)')
+	res.render('index.jsx')
 })
 
-app.post('/done',function(req,res){
+app.post('/',function(req,res){
 	var yourStatment = req.body.yourStatment;
-	res.send('index.jsx')
+	var repeats = req.body.repeats;
+	res.render('index.jsx')
 })
 
 app.listen(7000, function(){
